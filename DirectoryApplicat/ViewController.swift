dd462f058978947c9fda510672b28bf58637e3a7
Suraj <suraj756@gmail.com>
2022-03-21 21:59:53 +0530

    Initial Commit

---
+++ DirectoryApplicat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DirectoryApplicat
+//
+//  Created by SAISURAJ on 3/21/22.
+//  Copyright © 2022 SAISURAJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
